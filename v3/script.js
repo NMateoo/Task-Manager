74e4f703f9669dd632911b7ecc2c09aad7bf1c01
Nicolás Mateo <55945912+NMateoo@users.noreply.github.com>
2023-12-26 14:11:41 +0100

    1

--- v3/script.js
+++ v3/script.js
@@ -2,7 +2,8 @@ let tasks = [];
 const taskName = document.getElementById('name');
 const btnAdd = document.getElementById('add');
 const taskList = document.getElementById('taskList');
-let selectedTask = null;
+const err = document.getElementById('errText');
+let timeOut;
 
 function loadStorage() {
     if (!localStorage.getItem('tasks')) {
@@ -21,10 +22,14 @@ function add(task) {
 }
 
 function remove(task) {
-    const index = tasks.indexOf(task);
-    tasks.splice(index, 1);
-    updateLocalStorage();
-    removeDOM(index);
+    if (tasks.length === 0) {
+        taskList.style.display = 'none';
+    } else {
+        const index = tasks.indexOf(task);
+        tasks.splice(index, 1);
+        updateLocalStorage();
+        removeDOM(index);
+    }
 }
 
 function removeDOM(index) {
@@ -33,6 +38,19 @@ function removeDOM(index) {
 
 function updateLocalStorage() {
     localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+function displayErr(msg) {
+    err.parentNode.style.display = 'flex';
+    err.innerText = msg;
+    taskName.value = '';
+    taskName.focus();
+
+    clearTimeout(timeOut);
+
+    timeOut = setTimeout(() => {
+        err.parentNode.style.display = 'none';
+    }, 3000);
 }
 
 function createHTML(name) {
@@ -50,22 +68,38 @@ function createHTML(name) {
     newLi.appendChild(btnDelete);
     taskList.appendChild(newLi);
 
-    //Falta hacer el update
+    //Falta hacer el update (En proceso)
+    newLi.addEventListener('click', e => {
+        taskInput.disabled = !taskInput.disabled;
+        taskInput.focus();
+        let taskCopy = tasks;
+        let i = taskCopy.indexOf(name);
+        console.log(i + name);
+        console.log(taskCopy);
+
+    });
 
     btnDelete.addEventListener('click', e => {
         e.preventDefault();
+        e.stopPropagation();
         remove(name);
         console.log(tasks);
     });
 }
 
 btnAdd.addEventListener('click', e => {
-    e.preventDefault();
-    add(taskName.value);
-    taskName.value = '';
-    taskName.focus();
-    console.log(tasks);
-    localStorage.setItem('tasks', JSON.stringify(tasks));
+    if (taskName.value.trim() === '') {
+        displayErr('Please enter a task');
+    } else if (taskName.value.trim().length > 27) {
+        displayErr('Task name is too long');
+    } else {
+        e.preventDefault();
+        add(taskName.value);
+        taskName.value = '';
+        taskName.focus();
+        console.log(tasks);
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+    }
 });
 
 document.addEventListener('DOMContentLoaded', loadStorage);
