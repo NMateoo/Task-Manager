09f869d01fe28c4a210e22793f5025a66701fd08
Nicolás Mateo <55945912+NMateoo@users.noreply.github.com>
2023-12-31 18:18:30 +0100

    1

--- v4/TaskManager/src/App.jsx
+++ v4/TaskManager/src/App.jsx
@@ -1,12 +1,31 @@
 function App() { 
 
+  const addTask = e => {
+    e.preventDefault()
+    console.log('Boton pulsado')
+  }
+
+  const errDisplay = (msg) => {
+    msg = 'Pepe'
+    return msg
+  }
+
   return (
-    <main className="flex justify-center items-center h-screen bg-slate-200 flex-col">
-      <h1 className="font-bold text-4xl text-zinc-900 mb-9">Task Manager</h1>
-      <section className="flex justify-center items-center border-solid border-black border-2 p-5">
-        <input type="text" placeholder="Enter a task name" 
-        className="border border-white rounded p-2 w-full" />
-        <button type="button" className=" bg-sky-700 p-3 rounded ml-2 text-center w-24 text-white">Add task</button>
+    <main className="flex justify-center items-center text-center bg-slate-200 flex-col w-4/12 p-12 rounded shadow-inner">
+      <h1 className="font-bold text-4xl text-zinc-900 pb-5">Task Manager</h1>
+
+      <section className="text-red-500 text-xl text-center font-medium">
+        <span>Error</span>
+      </section>
+
+      <section className="flex justify-center items-center p-3">
+        <input type="text" placeholder="Enter a task name" autoFocus spellCheck="false"
+        className="border border-white rounded p-2 w-full focus:border-none" />
+        <button onClick={addTask} type="button" 
+        className=" bg-sky-700 p-3 rounded ml-2 text-center w-32 text-white font-medium
+        transition-all cursor-pointer hover:translate-y-1 hover:opacity-60 hover:shadow-md">
+          Add task
+        </button>
       </section>
     </main>
   )
